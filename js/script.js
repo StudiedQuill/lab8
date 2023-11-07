7d38e8086cef04bba364f9e618ba37c96440cfc7
StudiedQuill <jmaravillalemus@hawk.iit.edu>
2023-11-06 21:18:41 -0600

    Troubleshooting
    Found these errors I was trying to find the reason as to why the alert doesn't pop up.

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ function init(){
 //add your javascrip between these two lines of code
   var out = document.getElementById('entrybutton');
   
-  function output{
-    var in = document.getAttribute("entryinput");
+  function output(){
+    var in = document.getElementById('entryinput');
 	document.getElementById('textoutput').innerHTML = textbox.value;
-    alert("John Maravilla: " + in);
+    alert("John Maravilla: " + textbox.value);
   }
   
   out.addEventListener('click', output);
