31833c2a470383dbc7542349acf22685da089740
StudiedQuill <jmaravillalemus@hawk.iit.edu>
2023-11-06 20:52:34 -0600

    Added variable and alert
    Not sure what to do from here. Nothing is occurring when I click alert though. Made another function because the last line is calling all of init which means there must be another function in between.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 function init(){
 //add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
+  var out = document.getElementById('entrybutton');
+  function output{
+    var in = document.getAttribute("entryinput");
+    alert("John Maravilla: " + in);
+  }
+  out.addEventListener('click', output);
+}
 window.addEventListener('load', init);
