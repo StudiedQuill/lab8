e25e6f02e637c38123cae2c2e64be258cd962096
StudiedQuill <jmaravillalemus@hawk.iit.edu>
2023-11-06 21:14:41 -0600

    added last part
    Had to look at this in the final js because I still couldn't get it to work. File currently not working after finishing this.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 function init(){
 //add your javascrip between these two lines of code
   var out = document.getElementById('entrybutton');
+  
   function output{
     var in = document.getAttribute("entryinput");
+	document.getElementById('textoutput').innerHTML = textbox.value;
     alert("John Maravilla: " + in);
   }
+  
   out.addEventListener('click', output);
 }
+
 window.addEventListener('load', init);
