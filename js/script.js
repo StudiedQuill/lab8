5a8baac5242fb403bb7f82163a670427be145948
StudiedQuill <143019836+StudiedQuill@users.noreply.github.com>
2023-11-13 12:47:55 -0600

    Fixing spacing

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
 function init(){
 //add your javascrip between these two lines of code
   var out = document.getElementById('entrybutton');
-  
   function output(){
     var input = document.getElementById('entryinput');
-	document.getElementById('textoutput').innerHTML = input.value;
+    document.getElementById('textoutput').innerHTML = input.value;
     alert("John Maravilla: " + input.value);
   }
-  
   out.addEventListener('click', output);
 }
-
 window.addEventListener('load', init);
